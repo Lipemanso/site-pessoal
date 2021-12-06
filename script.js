b239f2d1ae2d4e25167422dc1e3d5c2953d3517f
Lipemanso <contatofemanso@gmail.com>
2021-12-05 23:14:33 -0300

    site 1.0

---
+++ script.js
@@ -0,0 +1,9 @@
+const btnEnviar = document.getElementById("btnEnviar");
+
+btnEnviar.setAttribute("onclick", limparCampos());
+
+limparCampos = () =>{
+    
+    let campos = document.querySelector("input");
+    campos = "";
+}
